1688f0527e69fc23228de7195ca4cfb9ffe990a9
dennis-sketch <dennis.kimani2@student.moringaschool.comgit config --global user.name dennis-sketch>
2023-09-24 23:05:31 +0300

    Done with toy problems

---
+++ index.js
@@ -0,0 +1,19 @@
+function calculateGrade(){
+    const studentMarks= parseFloat(prompt["Enter student grade(between 0 and 100:"])
+}
+let grade;
+if(studentGrade>79);{
+    grade = "A"
+}elseif(studentGrade>=60);{
+    grade = "B";
+}elseif(studentGrade>=50);{
+    grade ="C";
+}elseif(studentGrade>=40);{
+    grade = "D";
+}elseif();{
+    grade = "E";
+}
+
+alert(`Grade: ${grade}`);
+
+calculateGrade();
